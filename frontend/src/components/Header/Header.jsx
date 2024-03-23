ca23e9821cae35b85c70ec36227405747c99cc5c
Amit Adhikari <amitadhikari525252@gmail.com>
2024-03-23 22:08:57 +0530

    Create website Header

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+      <div className="header-contents">
+        <h2>Order your favourite food here</h2>
+        <p>Embark on a culinary adventure with us and savor the extraordinary flavors of our menu offerings. From classic comfort foods to innovative creations, there's something for every palate to enjoy. </p>
+        <button>View Menu</button>
+      </div>
+    </div>
+  )
+}
+
+export default Header
